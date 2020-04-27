44093fe6caeea28f44834ab75d53c50fe4484e22
Clark Walcott <cgwy9f@mail.missouri.edu>
2020-04-27 16:39:20 -0500

    detect motion working with optical flow. Still need to add maximizing functionality

--- src/load.py
+++ src/load.py
@@ -1,30 +1,36 @@
 import numpy as np
 import cv2, time
 
-# Takes two sequential images from the camera and saves them into numpy arrays
+# Takes n sequential images from a video and saves them into numpy arrays
+# \param video_src the file path of the video file to load from
 # \param wait_time the amount of time to wait between video captures
 # \return an object containing the two numpy arrays, None on failure
-def load_images(wait_time : int) -> tuple:
+def load_images(video_src : str, n : int, wait_time : int) -> tuple:
     video = None
-    video = cv2.VideoCapture(0)
+    video = cv2.VideoCapture(video_src)
     for i in range(5):
         if not video.isOpened():
-            video = cv2.VideoCapture(0)
+            video = cv2.VideoCapture(video_src)
             if(i == 4):
                 return None
     frames = []
-    for i in range(2):
+    
+    framespersec = 24     # video framerate
+    videostart = 20       # want video capture to start at 20 seconds
+    for i in range(framespersec * videostart):    # should be 288
+        video.read()
+    
+    for i in range(n):
         ret, frame = video.read()
         frames.append(frame)
-        if(i == 0):
-            time.sleep(wait_time)
-    for frame in frames:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     video.release()
-    return tuple(frames)
+    return frames
 
 def main():
-    print(load_images(5))
+    frames = load_images("../tests/test_videos/Mufasa.mp4", 10, 5)
+    cv2.imshow("Frame 0", frames[0])
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
